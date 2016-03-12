f8ae9badf5601d5dd5f94dd94cbf98cdcdf37948
Aliaksei Kirylchyk <akirilchik@exadel.com>
2016-03-12 14:53:58 +0300

    Change code style to smart tabs.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,64 +1,64 @@
 angular.module('app.controllers', [])
-  
-.controller('homeCtrl', function($scope) {
 
-})
-   
-.controller('nationalCtrl', function($scope) {
+	.controller('homeCtrl', function ($scope) {
 
-    // FAKE CONTENT FOR THE DEMO
-  $scope.timeline = [{
-  	date: new Date(),
-  	title: "Awesome picture",
-  	author:"John Mybeweeg",
-  	text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-  	type: "picture"
-  },{
-  	date: new Date(),
-  	title: "Look at my video!",
-  	author:"Miranda Smith",
-  	text: "Lorem ipsum dolor sit amet",
-  	type: "video"
+	})
 
-  },{
-  	date: new Date(),
-  	title: "I am here",
-  	author:"Ludo Anderson",
-  	text: "Lorem ipsum dolor sit amet",
-  	type: "location"
+	.controller('nationalCtrl', function ($scope) {
 
-  },{
-  	date: new Date(),
-  	title: "For my friends",
-		author:"Sara Orwell",
-  	text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-  	type: "text"
+		// FAKE CONTENT FOR THE DEMO
+		$scope.timeline = [{
+			date: new Date(),
+			title: "Awesome picture",
+			author: "John Mybeweeg",
+			text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+			type: "picture"
+		}, {
+			date: new Date(),
+			title: "Look at my video!",
+			author: "Miranda Smith",
+			text: "Lorem ipsum dolor sit amet",
+			type: "video"
 
-  }]
+		}, {
+			date: new Date(),
+			title: "I am here",
+			author: "Ludo Anderson",
+			text: "Lorem ipsum dolor sit amet",
+			type: "location"
 
-})
-   
-.controller('sovietCtrl', function($scope) {
+		}, {
+			date: new Date(),
+			title: "For my friends",
+			author: "Sara Orwell",
+			text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+			type: "text"
 
-})
-      
-.controller('loginCtrl', function($scope) {
+		}]
 
-})
-   
-.controller('signupCtrl', function($scope) {
+	})
 
-})
-   
-.controller('modernCtrl', function($scope) {
+	.controller('sovietCtrl', function ($scope) {
 
-})
+	})
 
-.controller('IconsCtrl', function($scope) {
+	.controller('loginCtrl', function ($scope) {
 
-})
-   
-.controller('addYourOpinionCtrl', function($scope) {
+	})
 
-})
+	.controller('signupCtrl', function ($scope) {
+
+	})
+
+	.controller('modernCtrl', function ($scope) {
+
+	})
+
+	.controller('IconsCtrl', function ($scope) {
+
+	})
+
+	.controller('addYourOpinionCtrl', function ($scope) {
+
+	})
  
--- www/js/routes.js
+++ www/js/routes.js
@@ -1,81 +1,79 @@
 angular.module('app.routes', [])
 
-.config(function($stateProvider, $urlRouterProvider) {
+	.config(function ($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-    
-  
+		// Ionic uses AngularUI Router which uses the concept of states
+		// Learn more here: https://github.com/angular-ui/ui-router
+		// Set up the various states which the app can be in.
+		// Each state's controller can be found in controllers.js
+		$stateProvider
 
-      .state('menu.home', {
-    url: '/page1',
-    views: {
-      'side-menu21': {
-        templateUrl: 'templates/home.html',
-        controller: 'homeCtrl'
-      }
-    }
-  })
 
-  .state('menu.national', {
-    url: '/page2',
-    views: {
-      'side-menu21': {
-        templateUrl: 'templates/national.html',
-        controller: 'nationalCtrl'
-      }
-    }
-  })
+			.state('menu.home', {
+				url: '/page1',
+				views: {
+					'side-menu21': {
+						templateUrl: 'templates/home.html',
+						controller: 'homeCtrl'
+					}
+				}
+			})
 
-  .state('menu.soviet', {
-    url: '/page3',
-    views: {
-      'side-menu21': {
-        templateUrl: 'templates/soviet.html',
-        controller: 'sovietCtrl'
-      }
-    }
-  })
+			.state('menu.national', {
+				url: '/page2',
+				views: {
+					'side-menu21': {
+						templateUrl: 'templates/national.html',
+						controller: 'nationalCtrl'
+					}
+				}
+			})
 
-  .state('menu', {
-    url: '/side-menu21',
-    templateUrl: 'templates/menu.html',
-    abstract:true
-  })
+			.state('menu.soviet', {
+				url: '/page3',
+				views: {
+					'side-menu21': {
+						templateUrl: 'templates/soviet.html',
+						controller: 'sovietCtrl'
+					}
+				}
+			})
 
-  .state('login', {
-    url: '/page4',
-    templateUrl: 'templates/login.html',
-    controller: 'loginCtrl'
-  })
+			.state('menu', {
+				url: '/side-menu21',
+				templateUrl: 'templates/menu.html',
+				abstract: true
+			})
 
-  .state('signup', {
-    url: '/page5',
-    templateUrl: 'templates/signup.html',
-    controller: 'signupCtrl'
-  })
+			.state('login', {
+				url: '/page4',
+				templateUrl: 'templates/login.html',
+				controller: 'loginCtrl'
+			})
 
-  .state('modern', {
-    url: '/page6',
-    templateUrl: 'templates/modern.html',
-    controller: 'modernCtrl'
-  })
+			.state('signup', {
+				url: '/page5',
+				templateUrl: 'templates/signup.html',
+				controller: 'signupCtrl'
+			})
 
-  .state('menu.addYourOpinion', {
-    url: '/page8',
-    views: {
-      'side-menu21': {
-        templateUrl: 'templates/addYourOpinion.html',
-        controller: 'addYourOpinionCtrl'
-      }
-    }
-  })
+			.state('modern', {
+				url: '/page6',
+				templateUrl: 'templates/modern.html',
+				controller: 'modernCtrl'
+			})
 
-$urlRouterProvider.otherwise('/page4')
+			.state('menu.addYourOpinion', {
+				url: '/page8',
+				views: {
+					'side-menu21': {
+						templateUrl: 'templates/addYourOpinion.html',
+						controller: 'addYourOpinionCtrl'
+					}
+				}
+			})
 
-  
+		$urlRouterProvider.otherwise('/page4')
 
-});
+
+	});

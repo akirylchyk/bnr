8c3864fd34bf0e942c037a88dfe79c9c51126d26
Aliaksei Kirylchyk <akirilchik@exadel.com>
2016-03-21 01:44:57 +0300

    Added discuss page.

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,18 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.services', 'app.directives', 'morphCarousel', 'readMore', 'ion-gallery', 'ionic-pullup'])
+angular.module('app',
+	[
+		'ionic',
+		'app.controllers',
+		'app.routes',
+		'app.services',
+		'app.directives',
+		'morphCarousel',
+		'readMore',
+		'ion-gallery',
+		'ionic-pullup'
+	])
 
 	.run(function ($ionicPlatform) {
 		$ionicPlatform.ready(function () {
